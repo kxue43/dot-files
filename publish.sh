6d3cd1d8a0a9f0e84521ed028d756f79b1391a33
kxue43 <xueke.kent@gmail.com>
2025-09-23 23:17:47 -0400

    feat: Make `publish.sh` interactive.

--- publish.sh
+++ publish.sh
@@ -1,29 +1,77 @@
-#!/bin/bash
+#!/usr/local/bin/bash
+
+set -e
 
 make-zip() {
-  git push && \
-  zip -r dot-files.zip \
-  .aws/config \
-  .aws/credentials \
-  .vim/autoload/plug.vim \
-  .git-prompt.sh \
-  .gitconfig \
-  .gitconfig-personal \
-  .gvimrc \
-  .vimrc \
-  .bash_logout \
-  .bash_profile \
-  .bashrc
+  git push
+
+  declare -a zfiles=(.vim/autoload/plug.vim .git-prompt.sh .gitconfig .gitconfig-personal .gvimrc .vimrc .bash_logout .bash_profile .bashrc)
+
+  if [[ "$1" == "--initial" ]]; then
+    zfiles+=(.aws/config .aws/credentials)
+
+    zip -r dot-files-initial.zip ${zfiles[@]}
+  else
+    zip -r dot-files.zip ${zfiles[@]}
+  fi
 }
 
-make-release() {
-  gh release create --latest ${1} dot-files.zip
+publish-release() {
+  make-zip
+  make-zip --initial
+
+  gh release create --latest ${1} dot-files.zip dot-files-initial.zip
+
   git pull
-  rm dot-files.zip
+
+  rm dot-files.zip dot-files-initial.zip
 }
 
 delete-release() {
   gh release delete --cleanup-tag ${1}
 }
 
-make-zip && make-release ${1}
+PS3="Option: "
+
+select name in publish-release delete-release "exit";
+  do
+    case $name in
+      publish-release)
+        chosen=$(echo -n $name | tr "-" " ")
+        echo "Chose to ${chosen}."
+
+        echo -n "New tag for release: "
+
+        read tag
+
+        publish-release $tag
+
+        break
+        ;;
+      delete-release)
+        chosen=$(echo -n $name | tr "-" " ")
+        echo "Chose to ${chosen}."
+
+        echo -n "Release tag to delete: "
+
+        read tag
+
+        delete-release $tag
+
+        break
+        ;;
+      exit)
+        echo "Exited without doing anything."
+
+        break
+        ;;
+      *)
+        cat << EOF
+Please choose:
+1) publish-release
+2) delete-release
+3) exit
+EOF
+        ;;
+    esac
+done
