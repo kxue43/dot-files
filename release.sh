9672bb01c3f4ccc2828578a8c40a37f6b6afc468
kxue43 <xueke.kent@gmail.com>
2025-09-30 13:34:48 -0400

    feat: Download `plug.vim` and `git-prompt.sh` for every build.

--- release.sh
+++ release.sh
@@ -2,13 +2,31 @@
 
 set -e
 
+_download_assets() {
+  mkdir -p ~+/.vim/autoload
+
+  if [ -f ~+/.vim/autoload/plug.vim ]; then
+    rm ~+/.vim/autoload/plug.vim
+  fi
+
+  if [ -f ~+/.git-prompt.sh ]; then
+    rm ~+/.git-prompt.sh
+  fi
+
+  curl -o ~+/.vim/autoload/plug.vim -L https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+  curl -o ~+/.git-prompt.sh -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+}
+
 _make_zip() {
   git push
 
-  local -a zfiles=(.gvimrc .vimrc .bash_logout .bash_profile .bashrc .fns.bashrc)
+  _download_assets
+
+  local -a zfiles=(.vim/autoload/plug.vim .git-prompt.sh .gvimrc .vimrc .bash_logout .bash_profile .bashrc .fns.bashrc)
 
   if [[ "$1" == "--initial" ]]; then
-    zfiles+=(.vim/autoload/plug.vim .git-prompt.sh .gitconfig .gitconfig-personal .aws/config .aws/credentials)
+    zfiles+=(.gitconfig .gitconfig-personal .aws/config .aws/credentials)
 
     zip -r dot-files-initial.zip "${zfiles[@]}"
   else
