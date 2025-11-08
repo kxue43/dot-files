511f960fd1b25c252c1813813124cfd65391a0b2
kxue43 <xueke.kent@gmail.com>
2025-11-08 12:57:31 -0500

    feat: `silverblue-dev-toolbox.sh` sets up a new toolbox container.

---
+++ silverblue-dev-toolbox.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+printf "\033[36m%s\033[0m\n" "Installing the development-tools group."
+sudo dnf group install -y development-tools
+
+printf "\033[36m%s\033[0m\n" "Installing essential utilities."
+sudo dnf install -y rlwrap jq
+
+printf "\033[36m%s\033[0m\n" "Installing coding tools."
+sudo dnf install -y vim-enhanced neovim ripgrep luarocks tmux pre-commit
+
+printf "\033[36m%s\033[0m\n" "Installing pyenv build dependencies"
+sudo dnf update -y vte-profile
+sudo dnf install -y zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz xz-devel libffi-devel findutils tk-devel libzstd-devel
+
+printf "\033[36m%s\033[0m\n" "Installing pyenv."
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+pushd ~/.pyenv && src/configure && make -C src && popd
+
+printf "\033[36m%s\033[0m\n" "Installing pipx."
+sudo dnf install -y pipx
+pipx ensurepath
+
+printf "\033[36m%s\033[0m\n" "Installing poetry."
+pipx install poetry
+pipx inject poetry poetry-plugin-export
+
+printf "\033[36m%s\033[0m\n" "Installing Go."
+sudo dnf install -y golang
+
+printf "\033[36m%s\033[0m\n" "Installing rustup."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+printf "\033[36m%s\033[0m\n" "Installing fnm."
+cargo install fnm
+
+printf "\033[36m%s\033[0m\n" "Installing AWS CLI v2."
+sudo dnf install -y awscli2
+
+printf "\033[36m%s\033[0m\n" "Installing GitHub CLI."
+sudo dnf install -y gh
+
+printf "\033[36m%s\033[0m\n" "Installing shellcheck."
+sudo dnf install -y shellcheck
+
+printf "\033[36m%s\033[0m\n" "Installing CLI tools written in Go."
+go install github.com/kxue43/cli-toolkit/cmd/toolkit@latest
+go install github.com/kxue43/cli-toolkit/cmd/toolkit-assume-role@latest
+go install github.com/kxue43/cli-toolkit/cmd/toolkit-serve-static@latest
+go install github.com/kxue43/cli-toolkit/cmd/toolkit-show-md@latest
+go install mvdan.cc/sh/v3/cmd/shfmt@latest
+go install golang.org/x/tools/cmd/godoc@latest
+go install github.com/air-verse/air@latest
